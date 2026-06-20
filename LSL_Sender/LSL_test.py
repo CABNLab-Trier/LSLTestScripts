cf127165f50c4ddd007a184c9efaa65b6464a11e
Kiki <Kirsten.I.Schwabe@gmail.com>
2026-06-20 14:10:58 +0200

    Added code for a stream sender through pylsl.

---
+++ LSL_Sender/LSL_test.py
@@ -0,0 +1,10 @@
+import pylsl
+import time
+
+pylsl.set_config_filename("cfg/amyTI.txt")
+
+info = pylsl.StreamInfo(name = "Triggers", type = "Markers", channel_count =  1, nominal_srate = 0,  channel_format = "string", source_id = "desktop_triggers" )
+channel = pylsl.StreamOutlet(info)
+while True:
+    channel.push_sample(["hello from the other side... i must have pinged a thousand times"])
+    time.sleep(2)
